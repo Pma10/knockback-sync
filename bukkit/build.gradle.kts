e74df7943e443e5bd5393966e6b542b1c514b9a5
Axionize WS1 <154778082+Axionize@users.noreply.github.com>
2026-07-11 14:20:36 +0000

    Fix Bukkit 26.2 runtime dependencies

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -40,11 +40,11 @@ dependencies {
 
     shadeThisThing(implementation("org.kohsuke:github-api:1.326")!!)
     if (shadePE) {
-        shadeThisThing(implementation("com.github.retrooper:packetevents-spigot:2.12.2+22c9961-SNAPSHOT")!!)
+        shadeThisThing(implementation("com.github.retrooper:packetevents-spigot:2.13.0")!!)
     } else {
-        compileOnly("com.github.retrooper:packetevents-spigot:2.12.2+22c9961-SNAPSHOT")
+        compileOnly("com.github.retrooper:packetevents-spigot:2.13.0")
     }
-    shadeThisThing(implementation("org.incendo:cloud-paper:2.0.0-beta.15")!!)
+    shadeThisThing(implementation("org.incendo:cloud-paper:2.0.0-beta.17")!!)
     shadeThisThing(implementation("org.incendo:cloud-core:2.0.0")!!)
 
     // Required for 1.14.4 support because gson is too old to have JosnParser.parseString()
